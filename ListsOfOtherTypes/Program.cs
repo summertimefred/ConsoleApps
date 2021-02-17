f04c4825c3fef36762962c557031ee690c0e362b
summertimefred <summertimefred@outlook.com>
2021-02-16 20:09:39 -0500

    lists

---
+++ ListsOfOtherTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsOfOtherTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            //var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            //var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            //fibonacciNumbers.Add(previous + previous2);
+
+            //foreach (var item in fibonacciNumbers)
+            //    Console.WriteLine(item);
+
+            while (fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+
+                foreach (var item in fibonacciNumbers)
+                    Console.WriteLine(item);
+            }
+
+            //Try and write the code to generate the first 20 numbers in the sequence. (As a hint, the 20th Fibonacci number is 6765.)
+
+
+        }
+    }
+}
---
+++ SortSearchIndex/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSearchIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<name>", "Brandon", "Matthew", "Fred" };
+
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            var index = names.IndexOf("Matthew");
+            if (index != -1)
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
